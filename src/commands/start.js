58d847fe7f21e607907ffa67934e24a747477987
RizaCode <ariza.sulthanrafi@gmail.com>
2026-03-01 17:15:03 +0700

    merapikan start

--- src/commands/start.js
+++ src/commands/start.js
@@ -2,20 +2,20 @@ module.exports = (bot) => {
   bot.onText(/\/start/, (msg) => {
   bot.sendMessage(
     msg.chat.id,
-    `🌙 Ramadhan Companion Bot 🇮🇩
+  `🌙 Ramadhan Companion Bot 🇮🇩
 
-    Assalamu’alaikum 🤍  
-    Aku siap menemani ibadahmu selama Ramadhan.
+  🤍 Assalamu’alaikum 🤍  
+  Aku siap menemani ibadahmu selama Ramadhan.
 
-    📌 Command:
-    /doa - Doa harian random  
-    /motivasi - Motivasi Ramadhan  
-    /setkota - Set lokasi otomatis  
-    /sholat - Jadwal sholat hari ini
-    /reminderon - Nyalakan reminder sahur, imsak & buka
-    /reminderoff - Matikan reminder sahur, imsak & buka 
+  📌 Command:
+  /doa - Doa harian random  
+  /motivasi - Motivasi Ramadhan  
+  /setkota - Set lokasi otomatis  
+  /sholat - Jadwal sholat hari ini
+  /reminderon - Nyalakan reminder sahur, imsak & buka
+  /reminderoff - Matikan reminder sahur, imsak & buka 
 
-    ⏰ Reminder: sahur, imsak & buka otomatis`,
+  ⏰ Reminder: sahur, imsak & buka otomatis`,
 
 { parse_mode: "Markdown" }
   );
